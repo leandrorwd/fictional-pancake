5f060273c8cb67a2b80c9700e576feb011f19c9c
13111249 <13111249@l3332129.portoalegre.pucrsnet.br>
2016-09-08 22:25:38 -0300

    Primeiro commit sem código.

---
+++ Persistence/src/persistence/Persistence.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistence;
+
+/**
+ *
+ * @author 13111249
+ */
+public class Persistence {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
